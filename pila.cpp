501f33153651a6f142182f5fe546bc43911b24e7
Madh93 <michelangelo93@gmail.com>
2014-09-30 20:11:57 +0100

    Añadiendo métodos

--- pila.cpp
+++ pila.cpp
@@ -1,2 +1,32 @@
 #include "pila.hpp"
 
+	template <class T>
+	Pila<T>::Pila(int sz){}
+
+
+	template <class T>
+	Pila<T>::~Pila(){}	
+
+
+	template <class T>
+	bool Pila<T>::empty(){}	
+
+
+	template <class T>
+	bool Pila<T>::full(){}	
+
+
+	template <class T>
+	void Pila<T>::push(T dato){}	
+
+
+	template <class T>
+	T Pila<T>::pop(){}	
+
+
+	template <class T>
+	T Pila<T>::top(){}		
+
+
+	template <class T>
+	void Pila<T>::imprimir(){}		
